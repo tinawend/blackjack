38d92f30543c9d618b510dfa2aee7c7592b39402
Tina Wendpaap <tw222eu@student.lnu.se>
2020-05-13 13:25:36 +0200

    starting ex3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace examination_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
